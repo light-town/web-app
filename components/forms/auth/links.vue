7cd1debd6656995878c13eeb8c3653c41b6e560b
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2021-03-24 19:21:20 +0300

    Added links to Sign-In page

---
+++ components/forms/auth/links.vue
@@ -0,0 +1,32 @@
+<template>
+  <ui-grid align-items="center" justify="space-between" class="links">
+    <ui-button variant="text" class="links__btn">
+      <ui-grid align-items="center">
+        <p>English (US)</p>
+        <arrow-icon class="links__icon"></arrow-icon>
+      </ui-grid>
+    </ui-button>
+    <ui-grid class="links__support">
+      <ui-button variant="text" class="links__btn">Help</ui-button>
+      <ui-button variant="text" class="links__btn">Privacy</ui-button>
+      <ui-button variant="text" class="links__btn">Terms</ui-button>
+    </ui-grid>
+  </ui-grid>
+</template>
+
+<script>
+import UiGrid from '~/ui/grid/index.vue';
+import UiButton from '~/ui/button/index.vue';
+import ArrowIcon from '~/assets/right-arrow.svg?inline';
+
+export default {
+  name: 'Links',
+  components: {
+    UiGrid,
+    UiButton,
+    ArrowIcon,
+  },
+};
+</script>
+
+<style lang="scss" src="./links.scss"></style>
--- pages/sign-in/index.vue
+++ pages/sign-in/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <ui-grid class="page-container" justify="center">
-    <ui-grid direction="column" align-items="center">
+  <ui-grid class="page__container" justify="center">
+    <ui-grid direction="column" align-items="center" class="page__form-layout">
       <logo></logo>
       <auth-form
         title="Sign In"
@@ -22,6 +22,7 @@
           <ui-button variant="contained" type="submit">Next</ui-button>
         </template>
       </auth-form>
+      <links-form></links-form>
     </ui-grid>
   </ui-grid>
 </template>
@@ -32,6 +33,7 @@ import UiInput from '~/ui/input/index.vue';
 import UiButton from '~/ui/button/index.vue';
 import Logo from '~/components/forms/auth/logo.vue';
 import AuthForm from '~/components/forms/auth/form.vue';
+import LinksForm from '~/components/forms/auth/links.vue';
 
 export default {
   name: 'SignInPage',
@@ -41,6 +43,7 @@ export default {
     UiButton,
     Logo,
     AuthForm,
+    LinksForm,
   },
   methods: {
     handleSubmitForm(e) {
