4392e8721ec953639b7f1c39af4de2512356c477
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2021-03-26 11:36:59 +0300

    Added support csrf protection

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,8 @@
-export default ({ $axios }) => {
+export default async ({ $axios }) => {
   $axios.defaults.baseURL = process.env.NUXT_ENV_API_HTTP_URL;
   $axios.defaults.withCredentials = true;
+
+  const response = await $axios.get('/auth/csrf-token');
+
+  $axios.setHeader('X-CSRF-TOKEN', response.data.data['X-CSRF-TOKEN']);
 };
