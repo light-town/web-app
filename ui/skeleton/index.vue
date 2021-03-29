59d48bc8c896573bbd5120edc8226878cbcf2964
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2021-03-29 20:36:35 +0300

    Added skeleton UI element

---
+++ ui/skeleton/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <span
+    :class="['ui-skeleton', 'ui-skeleton__pulse']"
+    :style="{ width, height, borderRadius: radius }"
+  >
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'UiSkeleton',
+  props: {
+    width: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      required: true,
+    },
+    radius: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss"></style>
