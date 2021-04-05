b0e8d9936a1470480c641d68fb82f422e7a486bf
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2021-04-05 14:09:34 +0300

    Added setting authorization access token and redirecting to /sign-in when 401 request error

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,23 @@
-export default async ({ $axios }) => {
+export default async ({ $axios, store, redirect }) => {
   $axios.defaults.baseURL = process.env.NUXT_ENV_API_HTTP_URL;
   $axios.defaults.withCredentials = true;
 
   const response = await $axios.get('/auth/csrf-token');
 
   $axios.setHeader('X-CSRF-TOKEN', response.data.data['X-CSRF-TOKEN']);
+
+  $axios.interceptors.request.use(req => ({
+    ...req,
+    headers: {
+      ...req.headers,
+      Authorization: `Bearer ${store.state.auth.session?.token}`,
+    },
+  }));
+
+  $axios.interceptors.response.use(res => {
+    if (res.status === 401) {
+      redirect('/sign-in');
+    }
+    return res;
+  });
 };
